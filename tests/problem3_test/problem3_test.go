6f3721ff2e3cd7d9c9b63ac9e62bea7081698ca2
Ritesh Kudalkar <kriteshkudalkar@gmail.com>
2025-06-08 17:30:52 +0530

    Add problem 3

---
+++ tests/problem3_test/problem3_test.go
@@ -0,0 +1,35 @@
+package problem3_test
+
+import (
+	"github.com/therealriteshkudalkar/go_practice/src/problem3"
+	"testing"
+)
+
+func TestCountPrimes(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"Normal Case",
+			args{10},
+			4,
+		},
+		{
+			"Edge Case",
+			args{2},
+			0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := problem3.CountPrimes(tt.args.n); got != tt.want {
+				t.Errorf("CountPrimes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
