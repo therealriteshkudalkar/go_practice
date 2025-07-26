a64d2c1c821e86262bfd52ec39921094005da4e8
Ritesh Kudalkar <kriteshkudalkar@gmail.com>
2025-07-26 21:13:30 +0530

    Complete Problem 5

--- src/problem5/problem5.go
+++ src/problem5/problem5.go
@@ -1,26 +1,59 @@
 package problem5
 
-import "math"
+import (
+	"container/heap"
+	"math"
+)
+
+type nodeWeight struct {
+	node   int
+	weight int
+}
+
+type nodeDistance struct {
+	node     int
+	distance int
+}
+
+type priorityQueue []*nodeDistance
+
+func (pq *priorityQueue) Len() int {
+	return len(*pq)
+}
+
+func (pq *priorityQueue) Less(i, j int) bool {
+	return (*pq)[i].distance > (*pq)[j].distance
+}
+
+func (pq *priorityQueue) Swap(i, j int) {
+	(*pq)[i], (*pq)[j] = (*pq)[j], (*pq)[i]
+}
+
+func (pq *priorityQueue) Push(x any) {
+	*pq = append(*pq, x.(*nodeDistance))
+}
+
+func (pq *priorityQueue) Peek() any {
+	n := pq.Len()
+	return (*pq)[n-1]
+}
+
+func (pq *priorityQueue) Pop() any {
+	n := pq.Len()
+	x := (*pq)[n-1]
+	*pq = (*pq)[0 : n-1]
+	return x
+}
 
 func NetworkDelayTime(times [][]int, n int, k int) int {
-	type NodeWeight struct {
-		node int
-		weight int
-	}
-
-	type NodeDistance struct {
-		node int
-		distance int
-	}
 
 	// Form an adjacency list for traversing nodes
-	adjList := make([][]NodeWeight, n)
+	adjList := make([][]nodeWeight, n)
 	for _, time := range times {
-		adjList[time[0] - 1] = append(adjList[time[0] - 1], NodeWeight{ time[1] - 1, time[2] })
+		adjList[time[0]-1] = append(adjList[time[0]-1], nodeWeight{time[1] - 1, time[2]})
 	}
 
 	// Create a visited array and a distance array
-	visited := make([]bool, n)
 	distances := make([]int, n)
 
 	for i := range n {
@@ -28,25 +61,34 @@ func NetworkDelayTime(times [][]int, n int, k int) int {
 	}
 
 	// Create a new empty queue
-	queue := make([]NodeDistance, 0)
-	queue = append(queue, NodeDistance{k - 1, 0})
-	visited[k - 1] = true
-	distances[k - 1] = 0
+	queue := priorityQueue{}
+	heap.Init(&queue)
+	heap.Push(&queue, &nodeDistance{k - 1, 0})
+	distances[k-1] = 0
 
 	for len(queue) != 0 {
+		// Remove the item from the queue
+		currentNode := heap.Pop(&queue).(*nodeDistance)
 
+		// Explore the neighbours of this item
+		for _, neighbour := range adjList[currentNode.node] {
+			if distances[neighbour.node] > currentNode.distance+neighbour.weight {
+				distances[neighbour.node] = currentNode.distance + neighbour.weight
+				heap.Push(&queue, &nodeDistance{neighbour.node, distances[neighbour.node]})
+			}
+		}
 	}
 
-	min := math.MaxInt
+	max := 0
 	for index, distance := range distances {
 		if distance == math.MaxInt {
 			return -1
 		}
 
-		if distance < min && index != k - 1 {
-			min = distance
+		if distance > max && index != k-1 {
+			max = distance
 		}
 	}
 
-	return min
+	return max
 }
--- tests/problem5_test/problem5_test.go
+++ tests/problem5_test/problem5_test.go
@@ -9,25 +9,35 @@ import (
 func TestNetworkDelayTime(t *testing.T) {
 	type args struct {
 		times [][]int
-		n int
-		k int
+		n     int
+		k     int
 	}
 
 	tests := []struct {
 		name string
-		arg args
+		arg  args
 		want int
-	} {
+	}{
 		{
 			"Test Case 1",
 			args{[][]int{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2},
 			2,
 		},
+		{
+			"Test Case 2",
+			args{[][]int{{1, 2, 1}}, 2, 1},
+			1,
+		},
+		{
+			"Test Case 2",
+			args{[][]int{{1, 2, 1}}, 2, 2},
+			-1,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			if got := problem5.NetworkDelayTime(test.arg.times, test.arg.n, test.args.k); got != test.want {
+			if got := problem5.NetworkDelayTime(test.arg.times, test.arg.n, test.arg.k); got != test.want {
 				t.Errorf("NetworkDelayTime() = %v, want %v", got, test.want)
 			}
 		})
